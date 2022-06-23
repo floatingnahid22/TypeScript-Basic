c9aac0639f9d7f699799200cfadf7afa685f6379
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-23 14:35:57 +0600

    explicit-union-types

--- output/script.js
+++ output/script.js
@@ -1,30 +1,18 @@
 "use strict";
-//type checking
-//variable
-// let playerName = "Masrafi";
-// let age = 35;
-// console.log(playerName);
-// age = 34;
-// console.log(age);
-//function
-// // function multiply(a, b) { ////it will give error but js will accept it
-// function multiply(a: number, b: number) {
-//   return a * b;
-// }
-// // console.log(multiply("nahid", 4)); //it will give error but js will accept it
-// console.log(multiply(3, 4));
+// variable
+// let a: string | number;
+let a; // : means types bole deya
 // array
-// let fruits = ["apple", "banana", "orange"];
-// fruits.push("mango");
-// // fruits.push(4); //Argument of type 'number' is not assignable to parameter of type 'string'
-// // if we declare it as mixed then it will accept
-// // let mixed = ["mango", 5, true];
-// // mixed.push(false);
-// // console.log(mixed);
-//object
-// let person = {
-//   name: "nahid",
-//   age: 25,
-//   isHuman: true,
-// };
-// // it won't take an object outside of this schema
+// let b: string[] = [];
+// // b.push(3); //it will give error
+// b.push("nahid");
+let b = [];
+b.push("nahid", 5);
+console.log(b);
+// object
+let c;
+c = {
+    name: "nahid", // whatever object we want to put it won't give error
+};
+// if we want to specify this object
+let d;
