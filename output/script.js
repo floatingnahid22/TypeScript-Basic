bebd92769bc44f79036c7ec1b8fdd4d290b4f330
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-23 14:19:40 +0600

    types basic

--- output/script.js
+++ output/script.js
@@ -1,3 +1,30 @@
 "use strict";
-const country = "I love Bangladesh";
-console.log(country);
+//type checking
+//variable
+// let playerName = "Masrafi";
+// let age = 35;
+// console.log(playerName);
+// age = 34;
+// console.log(age);
+//function
+// // function multiply(a, b) { ////it will give error but js will accept it
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// // console.log(multiply("nahid", 4)); //it will give error but js will accept it
+// console.log(multiply(3, 4));
+// array
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push("mango");
+// // fruits.push(4); //Argument of type 'number' is not assignable to parameter of type 'string'
+// // if we declare it as mixed then it will accept
+// // let mixed = ["mango", 5, true];
+// // mixed.push(false);
+// // console.log(mixed);
+//object
+// let person = {
+//   name: "nahid",
+//   age: 25,
+//   isHuman: true,
+// };
+// // it won't take an object outside of this schema
