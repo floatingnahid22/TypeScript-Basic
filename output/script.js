8cd1760545b94a0ccc5b9601d935f14845803418
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-26 00:07:50 +0600

    function-signatures pushed

--- output/script.js
+++ output/script.js
@@ -1,1 +1,15 @@
 "use strict";
+// let add: (x: number, y: number) => number;
+// add = (a: number, b: number) => {
+//   return a + b;
+// };
+let calculation;
+calculation = (a, b, c) => {
+    if (c === "add") {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+console.log(calculation(5, 6, "minus"));
