4362e0815503e46dc34ff7e1776cbbde85342a94
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-26 01:30:45 +0600

    enum pushed

--- src/script.ts
+++ src/script.ts
@@ -1,47 +1,21 @@
-// generics
+// enums
+enum RType {
+  SUCCESS,
+  FAILURE,
+  UNAUTHENTICATED,
+  FORBIDEN,
+}
 
-// const addID = <
-//   T extends {
-//     name: string;
-//     age: number;
-//   }
-// >(
-//   obj: T
-// ) => {
-//   let id = Math.floor(Math.random() * 100);
-//   return { ...obj, id };
-// };
-
-// let user = addID({
-//   name: "Masrafee",
-//   age: 35,
-//   country: "Bangladesh",
-// });
-
-// addID(user);
-
-// for interface- generics
 interface APIResponse<T> {
   status: number;
-  type: string;
+  type: RType;
   data: T;
 }
 
-// object
-
-const responseOne: APIResponse<object> = {
+const responseOne: APIResponse<string> = {
   status: 200,
-  type: "good",
-  data: {
-    name: "Test",
-    something: 300,
-  },
+  type: RType.FAILURE,
+  data: "test",
 };
 
-// string
-
-// const responseOne: APIResponse<string> = {
-//   status: 200,
-//   type: "good",
-//   data: "test",
-// };
+console.log(responseOne);
