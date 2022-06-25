8236fa90eb3f811f03f159000dff83b923fc3f92
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-26 01:23:08 +0600

    generics pushed

--- src/script.ts
+++ src/script.ts
@@ -1,30 +1,47 @@
-import { Player } from "./classes/Player.js";
-import { IsPlayer } from "./interfaces/IsPlayer.js";
+// generics
 
-let masrafee: IsPlayer;
-masrafee = new Player("Masrafee", 35, "Bangladesh");
-
-let sakib: IsPlayer;
-sakib = new Player("Sakib", 33, "Bangladesh");
-
-const players: IsPlayer[] = [];
-
-players.push(masrafee);
-players.push(sakib);
-
-// interface RectangleOptions {
-//   width: number;
-//   length: number;
-// }
-
-// function drawRectangle(options: RectangleOptions) {
-//   let width = options.width;
-//   let length = options.length;
-// }
-
-// let threeDOptions = {
-//   width: 20,
-//   length: 10,
-//   height: 10,
+// const addID = <
+//   T extends {
+//     name: string;
+//     age: number;
+//   }
+// >(
+//   obj: T
+// ) => {
+//   let id = Math.floor(Math.random() * 100);
+//   return { ...obj, id };
 // };
-// drawRectangle(threeDOptions);
+
+// let user = addID({
+//   name: "Masrafee",
+//   age: 35,
+//   country: "Bangladesh",
+// });
+
+// addID(user);
+
+// for interface- generics
+interface APIResponse<T> {
+  status: number;
+  type: string;
+  data: T;
+}
+
+// object
+
+const responseOne: APIResponse<object> = {
+  status: 200,
+  type: "good",
+  data: {
+    name: "Test",
+    something: 300,
+  },
+};
+
+// string
+
+// const responseOne: APIResponse<string> = {
+//   status: 200,
+//   type: "good",
+//   data: "test",
+// };
