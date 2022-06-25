f443b0d093dbd3a34827883a824958a4ebde3d43
floatingnahid22 <nahid.uap.github@gmail.com>
2022-06-26 01:38:44 +0600

    tuples pushed

--- output/script.js
+++ output/script.js
@@ -1,15 +1,7 @@
 "use strict";
-// enums
-var RType;
-(function (RType) {
-    RType[RType["SUCCESS"] = 0] = "SUCCESS";
-    RType[RType["FAILURE"] = 1] = "FAILURE";
-    RType[RType["UNAUTHENTICATED"] = 2] = "UNAUTHENTICATED";
-    RType[RType["FORBIDEN"] = 3] = "FORBIDEN";
-})(RType || (RType = {}));
-const responseOne = {
-    status: 200,
-    type: RType.FAILURE,
-    data: "test",
-};
-console.log(responseOne);
+// tuples
+let a = [3, "hello", { p: 3 }];
+// a[1] = 3 // possible because of not using tuples
+let b = [4, "world", { t: 1 }];
+// b[1] = 3 // cant possible because of tuples
+b.push();
